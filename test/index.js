4d451cb575d0321567ea0f18bd5007173865d0ca
Mark C Mitchell <mark@markcmitchell.net>
2016-12-10 17:06:37 +1100

    remove outdated tests, add test 'the stream.Readable will dispatch end event when directory listing is finished'

--- test/index.js
+++ test/index.js
@@ -23,63 +23,13 @@ tape.test('it will return a stream.Readable which will stream found directories'
   })
 })
 
-test.test('it will throw an error if no root directory is given', t => {
-  t.throws(getDirs, Error)
-  t.end()
-})
+tape.test('the stream.Readable will dispatch end event when directory listing is finished', t => {
+  const dirs = getDirs(testDir, readableStream => {
+    readableStream.on('data', buffer => {}) // required to flow stream
 
-test.test('it will throw an error if root directory does not exist', t => {
-  try {
-    getDirs('./this_folder_does_not_exist')
-  } catch(ex) {
-    t.equal(ex.code, 'ENOENT')
-  }
-  t.end()
-})
-
-test.test('it will ignore any directories that match the naming passed to exclude arg', t => {
-
-  let excludeDir = ['folderAA']
-  let dirs = getDirs(testDir, excludeDir)
-  t.deepEqual(dirs, [
-    `${testDir}/folderA`,
-    `${testDir}/folderB`
-  ])
-
-  excludeDir = ['folderAA', 'folderB']
-  dirs = getDirs(testDir, excludeDir)
-  t.deepEqual(dirs, [
-    `${testDir}/folderA`
-  ])
-
-  excludeDir = ['fold']
-  dirs = getDirs(testDir, excludeDir)
-  t.deepEqual(dirs, [])
-
-  t.end()
-
-})
-
-test.test('it will ignore any directories that match a passed-in RegEx object', t => {
-  fs.mkdirSync(`${testDir}/.secretFolder`)
-  fs.mkdirSync(`${testDir}/test.folder`)
-
-  const exclude = [/^\.\w+|\/\./] // match any root or nested /.dotFolders, nothing else
-  const dirs = getDirs(testDir, exclude)
-  t.deepEqual(dirs, [
-    `${testDir}/folderA`,
-    `${testDir}/folderA/folderAA`,
-    `${testDir}/folderB`,
-    `${testDir}/test.folder`
-  ])
-
-  fs.rmdirSync(`${testDir}/.secretFolder`)
-  fs.rmdirSync(`${testDir}/test.folder`)
-  t.end()
-})
-
-test.test('it will throw an error if something other than a string or RegEx is passed into exclusion array', t => {
-  const exclude = [1, 2, 3]
-  t.throws(getDirs.bind(null, testDir, exclude), Error)
-  t.end()
+    readableStream.on('end', () => {
+      t.pass('stream dispatched end event')
+      t.end()
+    })
+  })
 })
