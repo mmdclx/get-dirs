f10bea376fe2fe052e4bf048e93815e6d7365d53
Mark C Mitchell <mark@markcmitchell.net>
2016-12-10 15:53:24 +1100

    minor refactor to remove nested tests

--- test/index.js
+++ test/index.js
@@ -4,78 +4,74 @@ const getDirs = require('../index')
 
 const testDir = __dirname + '/testDirectory'
 
-test('get-dirs', t => {
+test.test('it will return, in an array, only directories', t => {
+  const dirs = getDirs(testDir)
 
-  t.test('it will return, in an array, only directories', t => {
-    const dirs = getDirs(testDir)
+  t.deepEqual(dirs, [
+    `${testDir}/folderA`,
+    `${testDir}/folderA/folderAA`,
+    `${testDir}/folderB`
+  ])
+  t.end()
+})
 
-    t.deepEqual(dirs, [
-      `${testDir}/folderA`,
-      `${testDir}/folderA/folderAA`,
-      `${testDir}/folderB`
-    ])
-    t.end()
-  })
+test.test('it will throw an error if no root directory is given', t => {
+  t.throws(getDirs, Error)
+  t.end()
+})
 
-  t.test('it will throw an error if no root directory is given', t => {
-    t.throws(getDirs, Error)
-    t.end()
-  })
+test.test('it will throw an error if root directory does not exist', t => {
+  try {
+    getDirs('./this_folder_does_not_exist')
+  } catch(ex) {
+    t.equal(ex.code, 'ENOENT')
+  }
+  t.end()
+})
 
-  t.test('it will throw an error if root directory does not exist', t => {
-    try {
-      getDirs('./this_folder_does_not_exist')
-    } catch(ex) {
-      t.equal(ex.code, 'ENOENT')
-    }
-    t.end()
-  })
+test.test('it will ignore any directories that match the naming passed to exclude arg', t => {
 
-  t.test('it will ignore any directories that match the naming passed to exclude arg', t => {
+  let excludeDir = ['folderAA']
+  let dirs = getDirs(testDir, excludeDir)
+  t.deepEqual(dirs, [
+    `${testDir}/folderA`,
+    `${testDir}/folderB`
+  ])
 
-    let excludeDir = ['folderAA']
-    let dirs = getDirs(testDir, excludeDir)
-    t.deepEqual(dirs, [
-      `${testDir}/folderA`,
-      `${testDir}/folderB`
-    ])
+  excludeDir = ['folderAA', 'folderB']
+  dirs = getDirs(testDir, excludeDir)
+  t.deepEqual(dirs, [
+    `${testDir}/folderA`
+  ])
 
-    excludeDir = ['folderAA', 'folderB']
-    dirs = getDirs(testDir, excludeDir)
-    t.deepEqual(dirs, [
-      `${testDir}/folderA`
-    ])
+  excludeDir = ['fold']
+  dirs = getDirs(testDir, excludeDir)
+  t.deepEqual(dirs, [])
 
-    excludeDir = ['fold']
-    dirs = getDirs(testDir, excludeDir)
-    t.deepEqual(dirs, [])
+  t.end()
 
-    t.end()
+})
 
-  })
+test.test('it will ignore any directories that match a passed-in RegEx object', t => {
+  fs.mkdirSync(`${testDir}/.secretFolder`)
+  fs.mkdirSync(`${testDir}/test.folder`)
 
-  t.test('it will ignore any directories that match a passed-in RegEx object', t => {
-    fs.mkdirSync(`${testDir}/.secretFolder`)
-    fs.mkdirSync(`${testDir}/test.folder`)
+  const exclude = [/^\.\w+|\/\./] // match any root or nested /.dotFolders, nothing else
+  const dirs = getDirs(testDir, exclude)
+  t.deepEqual(dirs, [
+    `${testDir}/folderA`,
+    `${testDir}/folderA/folderAA`,
+    `${testDir}/folderB`,
+    `${testDir}/test.folder`
+  ])
 
-    const exclude = [/^\.\w+|\/\./] // match any root or nested /.dotFolders, nothing else
-    const dirs = getDirs(testDir, exclude)
-    t.deepEqual(dirs, [
-      `${testDir}/folderA`,
-      `${testDir}/folderA/folderAA`,
-      `${testDir}/folderB`,
-      `${testDir}/test.folder`
-    ])
+  fs.rmdirSync(`${testDir}/.secretFolder`)
+  fs.rmdirSync(`${testDir}/test.folder`)
+  t.end()
+})
 
-    fs.rmdirSync(`${testDir}/.secretFolder`)
-    fs.rmdirSync(`${testDir}/test.folder`)
-    t.end()
-  })
-
-  t.test('it will throw an error if something other than a string or RegEx is passed into exclusion array', t => {
-    const exclude = [1, 2, 3]
-    t.throws(getDirs.bind(null, testDir, exclude), Error)
-    t.end()
-  })
-
+test.test('it will throw an error if something other than a string or RegEx is passed into exclusion array', t => {
+  const exclude = [1, 2, 3]
+  t.throws(getDirs.bind(null, testDir, exclude), Error)
+  t.end()
 })
